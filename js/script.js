a138f9a1a4ea8151876f365a88a5acc563d7c8d1
Robin Westerhoff <rwesterhoff@me.com>
2020-03-11 12:42:35 +0100

    Add proper comments.

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ const jobRoleSelect = document.querySelector('form select#title'),
 
 jobRoleTextField.style.display = "none";
 
+// Show or hide job role field.
 jobRoleSelect.addEventListener("change", event => {
     jobRoleOptions.forEach(option => {
         if (option.selected && option.text === "Other") {
@@ -36,9 +37,11 @@ const designSelect = document.querySelector('form select#design'),
     colorOptions = colorSelect.querySelectorAll('option'),
     newOption = document.createElement('option');
 
+// Reset color select to display 1st option and show or hide color select based on design selected.
 function resetColorSelect() {
     colorSelect[0].text = "Select color";
     colorSelect.selectedIndex = 0;
+
     if (designSelect.selectedIndex == 0) {
         colorSection.style.display = "none";
     } else {
@@ -46,6 +49,7 @@ function resetColorSelect() {
     }
 }
 
+// Show and hide color options based on selected design.
 function filterColorOptions(text, removeText, removeChars) {
     const regexTxt = new RegExp(removeText),
         regexChars = new RegExp(removeChars),
@@ -54,9 +58,9 @@ function filterColorOptions(text, removeText, removeChars) {
 
     colorOptions.forEach(option => {
         if (!option.text.includes(result)) {
-            // Disable for Safari
+            // Disable for Safari.
             option.disabled = true;
-            // Hide for all compatible browsers
+            // Hide for all compatible browsers.
             option.hidden = true;
         } else {
             option.disabled = false;
@@ -65,9 +69,11 @@ function filterColorOptions(text, removeText, removeChars) {
     })
 }
 
+// Initialize 1st option with instructions and reset.
 colorSelect.prepend(newOption);
 resetColorSelect();
 
+// Add upper functionality to design select.
 designSelect.addEventListener("change", event => {
     let i = event.target.selectedIndex;
 
@@ -84,9 +90,10 @@ let amountCosts = 0,
 const activityField = document.querySelector('.activities'),
     allCheckBoxes = activityField.querySelectorAll('input[type="checkbox"]');
 
-// add total in HTML
+// Add total costs in HTML.
 totalCosts.innerHTML = 'Total costs:<span class="total-costs">' + currency + amountCosts + '</span>';
 activityField.append(totalCosts);
+
 
 allCheckBoxes.forEach(checkBox => {
     let dayAndTime = checkBox.dataset.dayAndTime,
@@ -97,30 +104,27 @@ allCheckBoxes.forEach(checkBox => {
             pickedDate = pickedActivity.dataset.dayAndTime,
             pickedCost = pickedActivity.dataset.cost;
 
-        //check each checkbox
+        // Use picked date and compare to dataset from sibling checkboxes and disable if they are overlapping. 
         allCheckBoxes.forEach(checkBox => {
-            //get date and compare dataset siblings    
             if (pickedDate && checkBox.dataset.dayAndTime == pickedDate) {
-                //unchecked?
                 if (!checkBox.checked && pickedActivity.checked) {
-                    // style siblings as disabled
                     checkBox.parentNode.style.color = 'rgba(0,0,0,0.35)';
                     checkBox.disabled = true;
-                    // disabled?
                 } else if (checkBox.disabled && !pickedActivity.checked) {
-                    // keep or style all as default    
                     checkBox.parentNode.style.color = 'inherit';
                     checkBox.disabled = false;
                 }
             }
         });
 
+        // Decrease or increase costs based on selected activities.
         if (cost && pickedActivity.checked) {
             amountCosts += parseInt(pickedCost);
         } else if (cost && !pickedActivity.checked) {
             amountCosts -= parseInt(pickedCost);
         }
 
+        // Update HTML with total costs.
         document.querySelector('.total-costs').innerText = currency + amountCosts;
     })
 });
@@ -134,11 +138,14 @@ const selectPayment = document.querySelector('select#payment'),
     paymentPaypal = document.querySelector('#paypal'),
     paymentBitcoin = document.querySelector('#bitcoin');
 
+// Disable the 1st option with instructions.
 paymentMethodOption.disabled = true;
 
+// Hide all panels other than credit-card.
 paymentPaypal.style.display = 'none';
 paymentBitcoin.style.display = 'none';
 
+// Show and hide info panels / form based on payment select.
 selectPayment.addEventListener('change', () => {
     paymentCredit.style.display = 'none';
     paymentPaypal.style.display = 'none';
@@ -153,8 +160,9 @@ selectPayment.addEventListener('change', () => {
     }
 });
 
+// Set default selection to credit-card.
 selectPayment.selectedIndex = 1;
-console.log(selectPayment.selectedIndex);
+
 /*****************************************
 Form validation
 *****************************************/
@@ -165,6 +173,7 @@ const nameField = document.querySelector('input#name'),
     zipCode = document.querySelector('input#zip'),
     cvv = document.querySelector('input#cvv');
 
+// Speaks for itself.
 function validateName() {
     const regex = /^\D+\s?\w*$/i;
 
@@ -177,7 +186,6 @@ function validateEmail() {
     return regex.test(emailField.value);
 }
 
-// Validate activities
 function validateActivities() {
     for (let i = 0; i < allCheckBoxes.length; i++) {
         if (allCheckBoxes[i].checked) {
@@ -204,11 +212,13 @@ function validateCvv() {
     return regex.test(cvv.value);
 }
 
+// Add or remove error styling based on validation.
 function toggleError(validation, element, message) {
     validation ? element.classList.remove('error') : element.classList.add('error');
     errorMessage(validation, element, message);
 }
 
+// Add or remove error message based on validation.
 function errorMessage(validation, element, message) {
     const p = document.createElement('p'),
         sibling = element.nextElementSibling,
@@ -242,12 +252,12 @@ function errorMessage(validation, element, message) {
     }
 }
 
-// Validate namefield
+// Add validation to name field.
 nameField.addEventListener('blur', () => {
     toggleError(validateName(), nameField);
 });
 
-// Validate emailfield
+// Add validation to email field.
 emailField.addEventListener('input', () => {
     toggleError(validateEmail(), emailField, 'Please provide a valid email address.');
 });
@@ -255,22 +265,22 @@ emailField.addEventListener('blur', () => {
     toggleError(validateEmail(), emailField, 'Please provide a valid email address.');
 });
     
-
-// Validate credit card number
+// Add validation to credit card number field.
 creditCardNumber.addEventListener('blur', () => {
     toggleError(validateCreditCardName(), creditCardNumber, 'Please provide a number between 13 and 16 characters.');
 });
 
-// Validate zip code
+// Add validation to zip code field.
 zipCode.addEventListener('blur', () => {
     toggleError(validateZip(), zipCode, 'Please provide 5 numbers.');
 });
 
-// Validate zip code
+// Add validation to zip code field.
 cvv.addEventListener('blur', () => {
     toggleError(validateCvv(), cvv, 'Please provide 3 numbers.');
 });
 
+// Add validation to submission of form.
 submitButton.addEventListener('click', event => {
     event.preventDefault();
     toggleError(validateName(), nameField);
@@ -280,25 +290,3 @@ submitButton.addEventListener('click', event => {
     toggleError(validateZip(), zipCode);
     toggleError(validateCvv(), cvv);
 });
-
-/*
-Add good code comments
-*/
-
-
-/*****************************************
-EXCEEDS PART
-******************************************/
-
-/*Real-time Error Messages
-Program your form so that it provides a real-time validation error message for at least one text input field. Rather than providing an error message on submit, your form should check for errors and display messages as the user begins typing inside a text field. For example, if the user enters an invalid email address, the error appears as the user begins to type, and disappears as soon as the user has entered a complete and correctly formatted email address. You must accomplish this with your own JavaScript code. Do not rely on HTML5's built-in email validation.
-NOTE: If you implement the above exceeds requirements in your form, make sure you detail in your submission notes which input will have different error messages depending on the error, and which input will have "real time" validation messages, so your reviewer won't miss them by accident.
-*/
-
-/*
-NOTE: Getting an "Exceed Expectations" grade.
-
-See the rubric in the "How You'll Be Graded" tab above for details on what you need to receive an "Exceed Expectations" grade.
-Passing grades are final. If you try for the "Exceeds Expectations" grade, but miss an item and receive a “Meets Expectations” grade, you won’t get a second chance. Exceptions can be made for items that have been misgraded in review.
-Always mention in the comments of your submission or any resubmission, what grade you are going for. Some students want their project to be rejected if they do not meet all Exceeds Expectations Requirements, others will try for all the "exceeds" requirement but do not mind if they pass with a Meets Expectations grade. Leaving a comment in your submission will help the reviewer understand which grade you are specifically going for
-*/
